5c9c5e971c01b9a0137ebabe2a6dba03a5d3ff1a
Gradziu <k.gradek31@gmail.com>
2017-10-07 02:13:32 +0200

    końcówki projektu, reszta poprawek rwd + js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log('script');
+            $("#button").click(function () {
+                $(".nav").toggleClass("navi");
+                $("#button").toggleClass("button1").filter('[class=""]').removeAttr('class');
+            });
+
+            $(window).scroll(function () {
+                // declare variable
+                var topPos = $(this).scrollTop();
+
+                // if user scrolls down - show scroll to top button
+                if (topPos > 100) {
+                    $("header").addClass("slide_down");
+                    $(".contnet").addClass("slide");
+                } else {
+                    $("header").removeClass("slide_down");
+                    $("header .contnet").removeClass("slide");
+                }
+
+            });
+  
